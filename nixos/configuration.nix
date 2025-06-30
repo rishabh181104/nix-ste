42164c0c76cc322eb56534ecb047946b5b8b93be
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-07-01 02:30:10 +0530

    s

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,7 +22,7 @@
 # Use the systemd-boot EFI boot loader.
 # Lanzaboote replaces systemd-boot
   boot.loader.systemd-boot.enable = lib.mkForce false;
-  boot.loader.systemd-boot.enable = true;
+# boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 8;
   boot.loader.efi.canTouchEfiVariables = true;
 
